f09b8415911d53ebf181fdf8daf447ae6ed88e9c
Sybre Waaijer <sybrew@gmail.com>
2016-09-15 04:39:00 +0200

    Inpost SEO settings are put into view.
    This means the code is no longer loaded on the front-end.

---
+++ inc/views/inpost/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * When power leads men towards arrogance, poetry reminds him of his limitations.
+ * When power narrows the areas of man’s concern, poetry reminds him of the richness and diversity of existence.
+ * When power corrupts, poetry cleanses.
+ *
+ * - John F. Kennedy
+ */
