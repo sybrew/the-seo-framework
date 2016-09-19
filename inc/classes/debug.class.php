9a0d9f4735aaa8decfb9646ea6922a30e249a96c
pedro-mendonca <ped.gaspar@gmail.com>
2016-09-19 09:03:51 +0100

    Rebuild strings
    Reinclude the words "deprecated" and "incorrectly" in strings, keeping the `<strong>` html tags outside to guarantee that the words are highlighted.

--- inc/classes/debug.class.php
+++ inc/classes/debug.class.php
@@ -165,10 +165,10 @@ final class Debug implements Debug_Interface {
 
 			if ( isset( $replacement ) ) {
 				/* translators: 1: Function name, 2: 'Deprecated', 3: Plugin Version notification, 4: Replacement function */
-				trigger_error( sprintf( esc_html__( '%1$s is %2$s since version %3$s of The SEO Framework! Use %4$s instead.', 'autodescription' ), $function, __( '<strong>deprecated</strong>', 'autodescription' ), $version, $replacement ) );
+				trigger_error( sprintf( esc_html__( '%1$s is %2$sdeprecated%3$s since version %4$s of The SEO Framework! Use %5$s instead.', 'autodescription' ), $function, '<strong>', '</strong>', $version, $replacement ) );
 			} else {
 				/* translators: 1: Function name, 2: 'Deprecated', 3: Plugin Version notification */
-				trigger_error( sprintf( esc_html__( '%1$s is %2$s since version %3$s of The SEO Framework with no alternative available.', 'autodescription' ), $function, __( '<strong>deprecated</strong>', 'autodescription' ), $version ) );
+				trigger_error( sprintf( esc_html__( '%1$s is %2$sdeprecated%3$s since version %4$s of The SEO Framework with no alternative available.', 'autodescription' ), $function, '<strong>', '</strong>', $version ) );
 			}
 
 			restore_error_handler();
@@ -214,7 +214,7 @@ final class Debug implements Debug_Interface {
 
 			$version = empty( $version ) ? '' : sprintf( __( '(This message was added in version %s of The SEO Framework.)' ), $version );
 			/* translators: 1: Function name, 2: 'Incorrectly', 3: Plugin Version notification */
-			trigger_error( sprintf( esc_html__( '%1$s was called %2$s. %3$s', 'autodescription' ), esc_html( $function ), __( '<strong>incorrectly</strong>', 'autodescription' ), esc_html( $version ) ) );
+			trigger_error( sprintf( esc_html__( '%1$s was called %2$sincorrecly%3$s. %4$s', 'autodescription' ), esc_html( $function ), '<strong>', '</strong>', esc_html( $version ) ) );
 
 			restore_error_handler();
 		}
